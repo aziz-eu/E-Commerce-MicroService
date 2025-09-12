7f05830d766f3d782fe36c386bef2a25f7d2c3c7
Abdul Aziz <abdulaziz.eu1@gmail.com>
2025-09-12 10:21:00 +0600

    Add Auto Mapper and map ApplicationUser -> AuthenticationResponse

---
+++ ECommerceSolution.UserMicroService/eCommerce.Core/Mappers/ApplicationUserMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using eCommerce.Core.Dtos;
+using eCommerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Core.Mappers
+{
+    public class ApplicationUserMappingProfile: Profile
+    {
+        public ApplicationUserMappingProfile()
+        {
+            CreateMap<ApplicationUser, AuthenticationResponse>()
+                .ForMember(dest => dest.UserId, opt=> opt.MapFrom(dest=> dest.UserId))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest=> dest.Gender, opt => opt.MapFrom(src=> src.Gender))
+                .ForMember(dest => dest.IsAuthenticated, opt => opt.Ignore())
+                .ForMember(dest => dest.Token, opt => opt.Ignore())
+                ;
+        }
+    }
+}
