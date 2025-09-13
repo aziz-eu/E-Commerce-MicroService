7cad73017623484205454cdd102e4cadf3928195
infinityaziz <abdulaziz@infinitytechltd.com>
2025-09-13 14:55:53 +0600

    Create  IGeneric Repository and define methods

---
+++ eCommerce.ProductMicroServiceSolution/eCommerce.ProductMicroService.API/Data/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace eCommerce.ProductMicroService.API.Data
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task SaveChangesAsync();
+    }
+}
