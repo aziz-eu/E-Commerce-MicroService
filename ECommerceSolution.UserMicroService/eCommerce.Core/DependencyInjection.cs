a507efcd97fe177e5679b489f6ab0472174ee51e
Abdul Aziz <abdulaziz.eu1@gmail.com>
2025-09-13 07:25:41 +0600

    Add Fluent Validation for Login Dto

--- ECommerceSolution.UserMicroService/eCommerce.Core/DependencyInjection.cs
+++ ECommerceSolution.UserMicroService/eCommerce.Core/DependencyInjection.cs
@@ -1,22 +1,21 @@
 ﻿using eCommerce.Core.ServiceContracts;
 using eCommerce.Core.Services;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Linq;
-using System.Text;
+using FluentValidation;
 using System.Threading.Tasks;
+using eCommerce.Core.Validators;
+//using FluentValidation.DependencyInjectionExtensions;
+
 
 namespace eCommerce.Core
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddCore(this IServiceCollection services)
-        {
-            // Add core services here, e.g., domain services, business logic, etc.
-            // Example:
+        {           
             services.AddScoped<IUserService, UserService>();
+            services.AddValidatorsFromAssemblyContaining<LoginValidator>();
+
             return services;
         }
     }
---
+++ ECommerceSolution.UserMicroService/eCommerce.Core/Validators/LoginValidator.cs
@@ -0,0 +1,21 @@
+﻿using eCommerce.Core.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Core.Validators
+{
+    public class LoginValidator: AbstractValidator<LoginDto>
+    {
+
+        public LoginValidator()
+        {
+            RuleFor(x=> x.Email).NotEmpty().EmailAddress().WithMessage("Email Required");
+            RuleFor(x=> x.Password).NotEmpty().WithMessage("Email Required");
+        }
+
+    }
+}
