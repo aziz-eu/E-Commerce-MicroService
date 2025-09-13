7cb97a1610cdb27044c9a23c89efc82f3c3a3ca1
Abdul Aziz <abdulaziz.eu1@gmail.com>
2025-09-13 21:45:18 +0600

    Implement Product service

---
+++ eCommerce.ProductMicroServiceSolution/eCommerce.ProductMicroService.API/Services/ProductsServices.cs
@@ -0,0 +1,44 @@
+﻿using eCommerce.ProductMicroService.API.Data;
+using eCommerce.ProductMicroService.API.Entities;
+
+namespace eCommerce.ProductMicroService.API.Services
+{
+    public class ProductsServices
+    {
+        private readonly IGenericRepository<Product> _productsRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductsServices(IGenericRepository<Product> productsRepository, IUnitOfWork unitOfWork)
+        {
+            _productsRepository = productsRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IReadOnlyList<Product>> GetAllProductsAsync()
+        {
+            return await _productsRepository.GetAll();
+        }
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _productsRepository.Get(id);
+        }
+        public async Task<Product> CreateProductAsync(Product product)
+        {
+            var newProduct = await _productsRepository.Add(product);
+            await _unitOfWork.SaveAsync();
+            return newProduct;
+        }
+        public async Task UpdateProductAsync(Product product)
+        {
+            await _productsRepository.Update(product);
+            await _unitOfWork.SaveAsync();
+        }
+        public async Task DeleteProductAsync(int id)
+        {
+            var product = await _productsRepository.Get(id);
+            if (product != null)
+            {
+                await _productsRepository.Delete(product);
+                await _unitOfWork.SaveAsync();
+            }
+        }
+    }
+}
